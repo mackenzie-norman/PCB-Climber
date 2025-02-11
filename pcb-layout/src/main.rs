bd9cf21a8c4d35b7a4c7631462576efae75715d0
mackenzie-norman <115865992+mackenzie-norman@users.noreply.github.com>
2025-02-10 18:39:38 -0800

    commit b4 branch

--- pcb-layout/src/main.rs
+++ pcb-layout/src/main.rs
@@ -1,101 +1,108 @@
-use std::{collections::{btree_map, BTreeMap}, vec};
 use num::{cast::AsPrimitive, integer::gcd, ToPrimitive};
+use std::{
+    collections::{btree_map, BTreeMap},
+    vec,
+};
 mod plcmnt;
-use plcmnt::{Component, Bbox, Placement};
+use plcmnt::{Bbox, Component, Placement};
 use rand::prelude::*;
-
 
 //CHAT GPT CODE JUST TO TEST
 fn gcd_of_vector(nums: &[usize]) -> usize {
-
     let mut result = nums[0]; // Initialize with the first element
 
     for num in nums.iter().skip(1) {
-
         result = gcd(result, *num); // Calculate LCM for each pair
-
     }
 
     result
-
 }
-fn random_rotation() -> i32{
+fn random_rotation() -> i32 {
     // Get an RNG:
     let mut rng = rand::rng();
-    let opts = [90,180,270];
+    let opts = [90, 180, 270];
     let choice = opts.choose(&mut rng).unwrap();
     *choice
-    
 }
-struct Individual{
-    chromosone :  Vec<Vec<usize>>,
+struct Individual {
+    chromosone: Vec<Vec<usize>>,
     comp_list: Vec<Component>,
     discretization: usize,
 }
-impl Individual{
-    fn new( pl: Placement) -> Self{
+impl Individual {
+    fn new(pl: Placement) -> Self {
         //For now lets just say its a 6 x 6
         let mut sizes = Vec::new();
-        for a in &pl.components{
+        for a in &pl.components {
             sizes.push(a.get_height());
             sizes.push(a.get_width());
         }
         //println!("{:?}", sizes);
         let disc = gcd_of_vector(&sizes);
 
-        let mut a:BTreeMap<(usize, usize), usize > = BTreeMap::new();
+        let mut a: BTreeMap<(usize, usize), usize> = BTreeMap::new();
         let mut count: usize = 1;
-        for c in &pl.components{
+        for c in &pl.components {
             let mut c_space = c.bbox.as_btree(disc.try_into().unwrap(), count);
             a.append(&mut c_space);
             count += 1usize;
         }
-        
-        let mut csone_vec = Vec::new();
-        let y_end: usize = (pl.placement_area.y2/disc.to_i32().unwrap() ).try_into().unwrap();
-        let x_end: usize = (pl.placement_area.x2/disc.to_i32().unwrap()) .try_into().unwrap();
 
-        for y in 0..y_end{
+        let mut csone_vec = Vec::new();
+        let y_end: usize = (pl.placement_area.y2 / disc.to_i32().unwrap())
+            .try_into()
+            .unwrap();
+        let x_end: usize = (pl.placement_area.x2 / disc.to_i32().unwrap())
+            .try_into()
+            .unwrap();
+
+        for y in 0..y_end {
             let mut t_v = Vec::new();
-            for x in 0..x_end{
-                let coords : (usize,usize) = (x,y);
+            for x in 0..x_end {
+                let coords: (usize, usize) = (x, y);
                 match a.get(&coords) {
-                    Some(val) =>  t_v.push(*val),
-                    None => t_v.push(0)
+                    Some(val) => t_v.push(*val),
+                    None => t_v.push(0),
                 }
             }
             csone_vec.push(t_v)
         }
-        Individual{chromosone: csone_vec, comp_list: pl.components, discretization: disc}
+        Individual {
+            chromosone: csone_vec,
+            comp_list: pl.components,
+            discretization: disc,
+        }
     }
     fn to_tex(&self) {
-       for i in (&self.chromosone).into_iter().rev(){
-        println!("{:?}", i);
-       } 
+        for i in (&self.chromosone).into_iter().rev() {
+            println!("{:?}", i);
+        }
     }
 
-    fn is_valid(&self) -> bool{
+    fn is_valid(&self) -> bool {
         let mut valid: bool = true;
-        let mut a:BTreeMap<(usize, usize), usize > = BTreeMap::new();
+        let mut a: BTreeMap<(usize, usize), usize> = BTreeMap::new();
         let mut count: usize = 1;
-        
-        for c in &self.comp_list{
-            let mut c_space = (*c).bbox.as_btree(self.discretization.try_into().unwrap(), count);
+
+        for c in &self.comp_list {
+            let mut c_space = (*c)
+                .bbox
+                .as_btree(self.discretization.try_into().unwrap(), count);
             count += 1usize;
-        
-            for k in c_space.iter(){
-                let x = k.0.0 ;
-                let y = k.0.1 ;
+
+            for k in c_space.iter() {
+                let x = k.0 .0;
+                let y = k.0 .1;
                 let val = *k.1;
                 let try_val = self.chromosone[y][x];
-                if val != try_val{
+                if val != try_val {
                     return false;
                 }
             }
         }
-        valid 
+        valid
     }
-    /* 
+    /*
     fn get_locs(&self, search_val: usize) -> Vec<(usize,usize)>{
         let ret_v = Vec::new();
         let mut y: usize = 0;
@@ -111,101 +118,116 @@ impl Individual{
         }
     }
     */
-    fn swap(&mut self, a:usize, b:usize) {
-        let mut old_coords:BTreeMap<(usize, usize), usize > = BTreeMap::new();
-        let mut new_coords:BTreeMap<(usize, usize), usize > = BTreeMap::new();
+    fn swap(&mut self, a: usize, b: usize) {
+        let mut old_coords: BTreeMap<(usize, usize), usize> = BTreeMap::new();
+        let mut new_coords: BTreeMap<(usize, usize), usize> = BTreeMap::new();
 
-        //We need to zero, so lets grab the coords and also hold on to them 
-        let a_comp = &(self.comp_list[a-1]);
-        let mut c_space = (a_comp).bbox.as_btree(self.discretization.try_into().unwrap(), 0);
+        //We need to zero, so lets grab the coords and also hold on to them
+        let a_comp = &(self.comp_list[a - 1]);
+        let mut c_space = (a_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), 0);
         let old_a_loc = (a_comp.bbox.x1, a_comp.bbox.y1);
         old_coords.append(&mut c_space);
-        
-        let  b_comp = &(self.comp_list[b-1]);
-        let mut c_space = (b_comp).bbox.as_btree(self.discretization.try_into().unwrap(), 0);
+
+        let b_comp = &(self.comp_list[b - 1]);
+        let mut c_space = (b_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), 0);
         let old_b_loc = (b_comp.bbox.x1, b_comp.bbox.y1);
         old_coords.append(&mut c_space);
-        
+
         //Now we want to swap locations
-        let mut a_comp = &mut (self.comp_list[a-1]);
+        let mut a_comp = &mut (self.comp_list[a - 1]);
         a_comp.move_to(old_b_loc.0, old_b_loc.1);
-        let mut c_space = (a_comp).bbox.as_btree(self.discretization.try_into().unwrap(), a);
+        let mut c_space = (a_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), a);
         new_coords.append(&mut c_space);
 
-
-        let mut  b_comp = &mut (self.comp_list[b-1]);        
+        let mut b_comp = &mut (self.comp_list[b - 1]);
         b_comp.move_to(old_a_loc.0, old_a_loc.1);
-        let mut c_space = (b_comp).bbox.as_btree(self.discretization.try_into().unwrap(), b);
+        let mut c_space = (b_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), b);
         new_coords.append(&mut c_space);
-        for k in old_coords.iter(){
-            let x = k.0.0 ;
-            let y = k.0.1 ;
+        for k in old_coords.iter() {
+            let x = k.0 .0;
+            let y = k.0 .1;
             let val = k.1;
             self.chromosone[y][x] = *val;
         }
-        for k in new_coords.iter(){
-            let x = k.0.0 ;
-            let y = k.0.1 ;
+        for k in new_coords.iter() {
+            let x = k.0 .0;
+            let y = k.0 .1;
             let val = k.1;
             self.chromosone[y][x] = *val;
         }
-        
-        
-
-
-
-    
     }
-    fn rotate(& mut self, a:usize, rotation:i32){
-        let mut old_coords:BTreeMap<(usize, usize), usize > = BTreeMap::new();
-        let mut new_coords:BTreeMap<(usize, usize), usize > = BTreeMap::new();
+    fn rotate(&mut self, a: usize, rotation: i32) {
+        let mut old_coords: BTreeMap<(usize, usize), usize> = BTreeMap::new();
+        let mut new_coords: BTreeMap<(usize, usize), usize> = BTreeMap::new();
 
-        //We need to zero, so lets grab the coords and also hold on to them 
+        //We need to zero, so lets grab the coords and also hold on to them
         //let a: usize = 2;
-        
-        let mut a_comp = &mut (self.comp_list[a-1]);
-        let mut c_space = (a_comp).bbox.as_btree(self.discretization.try_into().unwrap(), 0);
+
+        let mut a_comp = &mut (self.comp_list[a - 1]);
+        let mut c_space = (a_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), 0);
         old_coords.append(&mut c_space);
         a_comp.rotate_comp(rotation);
-        
 
-        let mut c_space = (a_comp).bbox.as_btree(self.discretization.try_into().unwrap(), a);
+        let mut c_space = (a_comp)
+            .bbox
+            .as_btree(self.discretization.try_into().unwrap(), a);
         new_coords.append(&mut c_space);
-        for k in old_coords.iter(){
-            let x = k.0.0 ;
-            let y = k.0.1 ;
+        for k in old_coords.iter() {
+            let x = k.0 .0;
+            let y = k.0 .1;
             let val = k.1;
             self.chromosone[y][x] = *val;
         }
-        for k in new_coords.iter(){
-            let x = k.0.0 ;
-            let y = k.0.1 ;
+        for k in new_coords.iter() {
+            let x = k.0 .0;
+            let y = k.0 .1;
             let val = k.1;
             self.chromosone[y][x] = *val;
         }
-
     }
-    fn move_to_new(a:usize) {
-
-    }
+    fn move_to_new(a: usize) {}
 }
-
 
 fn main() {
     let placement_area = Bbox::new(0, 24, 0, 24);
-    let  boxx= Bbox::new(0,2,0,4);
-    let  mut c1 = Component{refdes: "C1".to_string(), bbox:boxx, rotation:0};
-    let  box2= Bbox::new(4,8,6,8);
-    let  mut c2 = Component{refdes: "C2".to_string(), bbox:box2, rotation:0};
-    let   box3= Bbox::new(4,12,0,6);
-    let  mut c3 = Component{refdes: "C3".to_string(), bbox:box3, rotation:0};
+    let boxx = Bbox::new(0, 2, 0, 4);
+    let mut c1 = Component {
+        refdes: "C1".to_string(),
+        bbox: boxx,
+        rotation: 0,
+    };
+    let box2 = Bbox::new(4, 8, 6, 8);
+    let mut c2 = Component {
+        refdes: "C2".to_string(),
+        bbox: box2,
+        rotation: 0,
+    };
+    let box3 = Bbox::new(4, 12, 0, 6);
+    let mut c3 = Component {
+        refdes: "C3".to_string(),
+        bbox: box3,
+        rotation: 0,
+    };
     c1.move_comp(6, 6);
     c2.move_comp(6, 6);
     c3.move_comp(6, 6);
     //c1.move_comp( 10, 11);
     //c1.rotate_comp(90);
-    let  comps:Vec<Component> = vec![c1,c2,c3];
-    let pl = Placement{components: comps, placement_area: placement_area };
+    let comps: Vec<Component> = vec![c1, c2, c3];
+    let pl = Placement {
+        components: comps,
+        placement_area: placement_area,
+    };
     let mut id = Individual::new(pl);
     //id.to_tex();
     //id.swap();
@@ -215,13 +237,10 @@ fn main() {
     id.rotate(3, 0);
     //id.swap(1, 3);
     //id.swap(1, 2);
-    
-    
-    let x = id.is_valid() ;
+
+    let x = id.is_valid();
     if x {
         id.to_tex();
     }
     //println!("{}",(c1.string()))
-    
-    
 }
--- pcb-layout/src/plcmnt.rs
+++ pcb-layout/src/plcmnt.rs
@@ -1,76 +1,75 @@
-use std::{collections::{ BTreeMap}, vec};
+use std::{collections::BTreeMap, vec};
 
 #[derive(Debug)]
-pub struct Bbox{
-    pub x1:i32,
-    pub x2:i32,
-    pub y1:i32,
-    pub y2:i32,
+pub struct Bbox {
+    pub x1: i32,
+    pub x2: i32,
+    pub y1: i32,
+    pub y2: i32,
     pub centerx: i32,
-    pub centery: i32
+    pub centery: i32,
 }
 
-impl Bbox{
-    pub fn new(x1:i32 ,x2:i32 ,y1:i32 ,y2:i32 ) -> Bbox{
-        Bbox { x1: x1, x2: x2, y1: y1, y2: y2, centerx: (x1-x2).abs() / 2, centery: (y1-y2).abs()/2 }
+impl Bbox {
+    pub fn new(x1: i32, x2: i32, y1: i32, y2: i32) -> Bbox {
+        Bbox {
+            x1: x1,
+            x2: x2,
+            y1: y1,
+            y2: y2,
+            centerx: (x1 - x2).abs() / 2,
+            centery: (y1 - y2).abs() / 2,
+        }
     }
-    pub fn get_width(&self) -> usize{
-        return (self.x1-self.x2).unsigned_abs().try_into().unwrap()
-
+    pub fn get_width(&self) -> usize {
+        return (self.x1 - self.x2).unsigned_abs().try_into().unwrap();
     }
-    pub fn get_height(&self) -> usize{
-        return (self.y1-self.y2).unsigned_abs().try_into().unwrap()
-        
+    pub fn get_height(&self) -> usize {
+        return (self.y1 - self.y2).unsigned_abs().try_into().unwrap();
     }
-    pub fn as_btree(&self, disc:i32, value:usize) -> BTreeMap<(usize, usize), usize> {
-        let mut ret_btree: BTreeMap <(usize,usize), usize> = BTreeMap::new();
-        let start_x = self.x1/disc;
-        let start_y = self.y1/disc;
-        let end_x = self.x2/disc;
-        let end_y = self.y2/disc;
+    pub fn as_btree(&self, disc: i32, value: usize) -> BTreeMap<(usize, usize), usize> {
+        let mut ret_btree: BTreeMap<(usize, usize), usize> = BTreeMap::new();
+        let start_x = self.x1 / disc;
+        let start_y = self.y1 / disc;
+        let end_x = self.x2 / disc;
+        let end_y = self.y2 / disc;
         let mut cur_x = start_x;
         let mut cur_y = start_y;
-        while cur_x <  end_x{
-            while cur_y < end_y{
+        while cur_x < end_x {
+            while cur_y < end_y {
                 let tmp_dict = (cur_x.try_into().unwrap(), cur_y.try_into().unwrap());
                 ret_btree.insert(tmp_dict, value);
                 cur_y += 1;
             }
             cur_x += 1;
             cur_y = start_y;
-
         }
         ret_btree
     }
-    /// Rotates around the x1,y1 to avoid nasty discretization issues. 
-    pub fn rotate(&mut self, angle: i32 ){
-        match angle{
-            90 => 
-            {
+    /// Rotates around the x1,y1 to avoid nasty discretization issues.
+    pub fn rotate(&mut self, angle: i32) {
+        match angle {
+            90 => {
                 let height = self.get_height();
                 let width = self.get_width();
                 self.x2 = self.x1 + height as i32;
                 self.y2 = self.y1 + width as i32;
-            },
-            180 => 
-            {
+            }
+            180 => {
                 let height = self.get_height();
                 let width = self.get_width();
                 //self.x1 = self.x2
-                
+
                 self.y2 = self.y1;
                 self.y1 = self.y1 - height as i32;
                 self.x2 = self.x1 + width as i32;
-                
-            },
+            }
 
-
-            270 =>
-            {
+            270 => {
                 let height = self.get_height();
                 let width = self.get_width();
                 self.x2 = self.x1;
-                self.y2 = self.y1; 
+                self.y2 = self.y1;
                 self.x1 = self.x2 - height as i32;
                 self.y1 = self.y2 - width as i32;
             }
@@ -78,41 +77,44 @@ impl Bbox{
         }
     }
 }
-pub struct Placement{
-    pub components : Vec<Component>,
-    pub placement_area: Bbox
-    
+pub struct Placement {
+    pub components: Vec<Component>,
+    pub placement_area: Bbox,
 }
 #[derive(Debug)]
-pub struct Component{
-    pub refdes : String,
+pub struct Component {
+    pub refdes: String,
     pub bbox: Bbox,
     pub rotation: i32,
 }
-impl Component{
-    fn string(&self) -> String{
-        return  self.refdes.clone() + " at (" + &self.bbox.centerx.to_string()  +"," + &self.bbox.centery.to_string() + ")";
+impl Component {
+    fn string(&self) -> String {
+        return self.refdes.clone()
+            + " at ("
+            + &self.bbox.centerx.to_string()
+            + ","
+            + &self.bbox.centery.to_string()
+            + ")";
     }
-    pub fn move_comp(&mut self,x:i32 ,y:i32){
+    pub fn move_comp(&mut self, x: i32, y: i32) {
         self.bbox.x1 += x;
         self.bbox.y1 += y;
         self.bbox.x2 += x;
         self.bbox.y2 += y;
     }
-    pub fn rotate_comp(& mut self, delta: i32){
+    pub fn rotate_comp(&mut self, delta: i32) {
         self.rotation += delta;
         self.bbox.rotate(delta);
     }
-    pub fn get_width(&self) -> usize{
-        return self.bbox.get_width()
+    pub fn get_width(&self) -> usize {
+        return self.bbox.get_width();
     }
-    pub fn get_height(&self) -> usize{
-        return self.bbox.get_height()
+    pub fn get_height(&self) -> usize {
+        return self.bbox.get_height();
     }
-    pub fn move_to(&mut self,x:i32 ,y:i32){
-        let delta_x = x - self.bbox.x1 ;
-        let delta_y = y - self.bbox.y1 ;
+    pub fn move_to(&mut self, x: i32, y: i32) {
+        let delta_x = x - self.bbox.x1;
+        let delta_y = y - self.bbox.y1;
         self.move_comp(delta_x, delta_y);
     }
-
 }
